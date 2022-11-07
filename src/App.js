d33b907e94cbe5170f0b0cfd48b03a40e1c2ad0c
Miguel Ramos Matos <miguel-r-matos@telecom.pt>
2022-11-07 10:48:05 +0000

    added card feature.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { Component, useState}  from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  function showCartHandler() {
+    setCartIsShown(true);
+  }
+
+  function hideCardHandler() {
+    setCartIsShown(false);
+  }
+
   return (
     <CartProvider>
-      <Header />
+      <Cart></Cart>
+      <Header  onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
